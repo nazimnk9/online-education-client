78619e6eb9e72de2afa2b87d1bcb39fe5b311883
nazimnk9 <nazimahmedprovat@gmail.com>
2023-07-25 20:51:26 +0600

    htp

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@
 // import 'antd/dist/antd.css';
 // import '../public/css/styles.css';
 // import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "../context";
 
 // function MyApp ({Component, pageProps}) {
