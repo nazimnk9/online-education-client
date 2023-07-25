f3957dbad381ce0c15b79e3881a26ca3f4957e39
nazimnk9 <nazimahmedprovat@gmail.com>
2023-07-25 20:54:24 +0600

    new

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'antd/dist/antd.css';
 // import '../public/css/styles.css';
-// import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "../context";
 
