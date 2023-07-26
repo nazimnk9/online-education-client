275417545d8955b458b0eae895e5b56d8aaa57f2
nazimnk9 <nazimahmedprovat@gmail.com>
2023-07-27 00:33:27 +0600

    up

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import CourseCard from "../components/cards/CourseCard";
 
-const Index = ({ courses }) => {
+let Index = ({ courses }) => {
      const [courses, setCourses] = useState([]);
 
      useEffect(() => {
